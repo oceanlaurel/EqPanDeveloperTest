66b5e78519a05093987a5ace506c1bfaf0c8392d
Lawrence Wong <oceandeep@gmail.com>
2023-11-09 20:40:51 +0000

    Completed

---
+++ src/test/java/com/pancredit/EqPancreditDeveloperTest/EqPancreditDeveloperTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pancredit.EqPancreditDeveloperTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EqPancreditDeveloperTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
